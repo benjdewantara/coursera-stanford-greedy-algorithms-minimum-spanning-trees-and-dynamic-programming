410167a42daf59b7ee5ede1d57cdf607e1a5d4cd
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-13 22:04:20 +0700

    Combine a pair of bit sum

--- week2/week2-ps/Main2/main2.go
+++ week2/week2-ps/Main2/main2.go
@@ -14,11 +14,45 @@ func Main2() {
 
 	edgeBitmap.SortAndDetermineBitSumIndicesRange()
 
-	for edgeCost := 1; edgeCost <= edgeBitmap.GetNumBitPerNode()/2; edgeCost++ {
-		existingEdgeCost := edgeBitmap.BitSumIndicesRange[edgeCost]
-		if existingEdgeCost == nil {
+	for bitSumNodeA := 0; bitSumNodeA < edgeBitmap.GetNumBitPerNode()/2; bitSumNodeA++ {
+		for bitSumNodeB := bitSumNodeA; bitSumNodeB < edgeBitmap.GetNumBitPerNode()/2; bitSumNodeB++ {
+			bitSumNodeARange := edgeBitmap.BitSumIndicesRange[bitSumNodeA]
+			bitSumNodeBRange := edgeBitmap.BitSumIndicesRange[bitSumNodeB]
+
+			bitSumNodeAExists := bitSumNodeARange != nil
+			bitSumNodeBExists := bitSumNodeBRange != nil
+
+			if !bitSumNodeAExists {
+				break
+			} else if !bitSumNodeBExists {
+				continue
+			}
+
+			nodeAStartIndx, nodeAEndIndx := bitSumNodeARange[0], bitSumNodeARange[1]
+			nodeBStartIndx, nodeBEndIndx := bitSumNodeBRange[0], bitSumNodeBRange[1]
+			for nodeAIdx := nodeAStartIndx; nodeAIdx <= nodeAEndIndx; nodeAIdx++ {
+				for nodeBIdx := nodeBStartIndx; nodeBIdx <= nodeBEndIndx; nodeBIdx++ {
+					if nodeAIdx == nodeBIdx {
+						continue
+					}
+
+
+				}
+			}
+		}
+
+		if edgeBitmap.BitSumIndicesRange[bitSumNodeA] == nil {
 			continue
 		}
+
+		//bitSumNodeB1 := edgeCostTarget - bitSumNodeA
+		//bitSumNodeB2 := edgeCostTarget + bitSumNodeA
+/*		if edgeBitmap.BitSumIndicesRange[bitSumNodeB1] == nil {
+			continue
+		} else if bitSumNodeB1 > edgeBitmap.GetNumBitPerNode() {
+			break
+		}
+*/
 	}
 
 	fmt.Println("asdf")
