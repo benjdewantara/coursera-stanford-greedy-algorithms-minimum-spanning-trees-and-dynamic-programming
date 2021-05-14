d90343f61e71bfa596e72340a58f893f975cf36a
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-14 22:50:31 +0700

    Merge all nodes with spacing 1 first, then all nodes with spacing 2

--- week2/week2-ps/Main2/main2.go
+++ week2/week2-ps/Main2/main2.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Main2() {
-	DISTANCE_AT_MOST := 2
+	//DISTANCE_AT_MOST := 2
 
 	edgeBitmap := EdgeBitmap.ReadProblem2Textfile("_fe8d0202cd20a808db6a4d5d06be62f4_clustering_big.txt")
 
@@ -16,62 +16,59 @@ func Main2() {
 
 	edgeBitmap.SortAndDetermineBitSumIndicesRange()
 
-	for bitSumNodeA := 0; bitSumNodeA < edgeBitmap.GetNumBitPerNode()/2; bitSumNodeA++ {
-		for bitSumNodeB := bitSumNodeA; bitSumNodeB < edgeBitmap.GetNumBitPerNode()/2; bitSumNodeB++ {
-			if !IsPossibleToFormDistanceAtMost(bitSumNodeA, bitSumNodeB, DISTANCE_AT_MOST) {
-				continue
-			}
+	for targetCost := 1; targetCost <= 2; targetCost++ {
+		for bitSumNodeA := 0; bitSumNodeA < edgeBitmap.GetNumBitPerNode()/2; bitSumNodeA++ {
+			for bitSumNodeB := bitSumNodeA; bitSumNodeB < edgeBitmap.GetNumBitPerNode()/2; bitSumNodeB++ {
+				if !IsPossibleToFormDistanceAtMost(bitSumNodeA, bitSumNodeB, targetCost) {
+					continue
+				}
 
-			bitSumNodeARange := edgeBitmap.BitSumIndicesRange[bitSumNodeA]
-			bitSumNodeBRange := edgeBitmap.BitSumIndicesRange[bitSumNodeB]
+				bitSumNodeARange := edgeBitmap.BitSumIndicesRange[bitSumNodeA]
+				bitSumNodeBRange := edgeBitmap.BitSumIndicesRange[bitSumNodeB]
 
-			bitSumNodeAExists := bitSumNodeARange != nil
-			bitSumNodeBExists := bitSumNodeBRange != nil
+				bitSumNodeAExists := bitSumNodeARange != nil
+				bitSumNodeBExists := bitSumNodeBRange != nil
 
-			if !bitSumNodeAExists {
-				break
-			} else if !bitSumNodeBExists {
-				continue
-			}
+				if !bitSumNodeAExists {
+					break
+				} else if !bitSumNodeBExists {
+					continue
+				}
 
-			nodeAStartIndx, nodeAEndIndx := bitSumNodeARange[0], bitSumNodeARange[1]
-			nodeBStartIndx, nodeBEndIndx := bitSumNodeBRange[0], bitSumNodeBRange[1]
-			for nodeAIdx := nodeAStartIndx; nodeAIdx <= nodeAEndIndx; nodeAIdx++ {
-				for nodeBIdx := nodeBStartIndx; nodeBIdx <= nodeBEndIndx; nodeBIdx++ {
-					if nodeAIdx == nodeBIdx {
-						continue
+				nodeAStartIndx, nodeAEndIndx := bitSumNodeARange[0], bitSumNodeARange[1]
+				nodeBStartIndx, nodeBEndIndx := bitSumNodeBRange[0], bitSumNodeBRange[1]
+				for nodeAIdx := nodeAStartIndx; nodeAIdx <= nodeAEndIndx; nodeAIdx++ {
+					for nodeBIdx := nodeBStartIndx; nodeBIdx <= nodeBEndIndx; nodeBIdx++ {
+						if nodeAIdx == nodeBIdx {
+							continue
+						}
+						nodeABits := edgeBitmap.Arr[nodeAIdx]
+						nodeBBits := edgeBitmap.Arr[nodeBIdx]
+						distance := EdgeBitmap.GetDistanceBetween(nodeABits, nodeBBits)
+
+						if !(distance == targetCost) {
+							continue
+						}
+						nodeA := nodeAIdx + 1
+						nodeB := nodeBIdx + 1
+
+						nodeALeader := unionFinder.GetNodeLeader(nodeA)
+						nodeBLeader := unionFinder.GetNodeLeader(nodeB)
+
+						if nodeALeader == nodeBLeader {
+							continue
+						}
+
+						nodeLeader, nodeFollower := nodeALeader, nodeBLeader
+						if nodeFollower < nodeLeader {
+							nodeLeader, nodeFollower = nodeFollower, nodeLeader
+						}
+						unionFinder.AssociateNodeWithLeader(nodeFollower, nodeLeader)
+						unionFinder.MergeFollowers(nodeLeader, nodeFollower)
 					}
-					nodeABits := edgeBitmap.Arr[nodeAIdx]
-					nodeBBits := edgeBitmap.Arr[nodeBIdx]
-					distance := EdgeBitmap.GetDistanceBetween(nodeABits, nodeBBits)
-
-					if !(distance <= DISTANCE_AT_MOST) {
-						continue
-					}
-					nodeA := nodeAIdx + 1
-					nodeB := nodeBIdx + 1
-
-					nodeALeader := unionFinder.GetNodeLeader(nodeA)
-					nodeBLeader := unionFinder.GetNodeLeader(nodeB)
-
-					if nodeALeader == nodeBLeader {
-						continue
-					}
-
-					nodeLeader, nodeFollower := nodeALeader, nodeBLeader
-					if nodeFollower < nodeLeader {
-						nodeLeader, nodeFollower = nodeFollower, nodeLeader
-					}
-					unionFinder.AssociateNodeWithLeader(nodeFollower, nodeLeader)
-					unionFinder.MergeFollowers(nodeLeader, nodeFollower)
 				}
 			}
 		}
-
-		if edgeBitmap.BitSumIndicesRange[bitSumNodeA] == nil {
-			continue
-		}
-
 	}
 
 	fmt.Println(fmt.Sprintf("len(unionFinder.LeadersDistinct) = %d", len(unionFinder.LeadersDistinct)))
