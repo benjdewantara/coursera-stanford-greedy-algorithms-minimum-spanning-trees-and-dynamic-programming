f6a1483fd2eebefd7f0fd959155df0558b87a553
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-18 08:01:54 +0700

    Week 3 No 1 Read textfile

--- week3/week3-ps/main.go
+++ week3/week3-ps/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+    "strconv"
+    "strings"
+)
 
 func main() {
-	fmt.Println("Hell on earth")
+    arr := ReadTextfile("_eed1bd08e2fa58bbe94b24c06a20dcdb_huffman.txt")
+
+    if arr != nil {
+    }
+
+    fmt.Println("Hell on earth")
+}
+
+func ReadTextfile(filepath string) *[]int {
+    contentBytes, _ := ioutil.ReadFile(filepath)
+    var arr []int
+
+    for lineIndx, intStr := range strings.Split(string(contentBytes), "\n") {
+        if arr == nil {
+            numNodes, _ := strconv.Atoi(intStr)
+            arr = make([]int, numNodes)
+            continue
+        }
+
+        if intStr == "" {
+            continue
+        }
+
+        num, _ := strconv.Atoi(intStr)
+        arr[lineIndx-1] = num
+    }
+
+    return &arr
 }
