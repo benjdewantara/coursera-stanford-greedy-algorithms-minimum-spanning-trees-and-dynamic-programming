69cd6443ae2da7f761e6bf4d5441ed052d075896
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-18 08:39:56 +0700

    Week 3 No 1 WeightLabel to keep track of the names

--- week3/week3-ps/ArrHeap/ArrHeap.go
+++ week3/week3-ps/ArrHeap/ArrHeap.go
@@ -1,40 +1,46 @@
 package ArrHeap
 
 import (
+    "fmt"
     "io/ioutil"
     "strconv"
     "strings"
 )
 
 type ArrHeap struct {
-    Arr []int
+    ArrWeight []int
+    ArrLabel  []string
 }
 
 func (a *ArrHeap) Len() int {
-    return len(a.Arr)
+    return len(a.ArrWeight)
 }
 
 func (a *ArrHeap) Less(i, j int) bool {
-    return a.Arr[i] < a.Arr[j]
+    return a.ArrWeight[i] < a.ArrWeight[j]
 }
 
 func (a *ArrHeap) Swap(i, j int) {
-    a.Arr[i], a.Arr[j] = a.Arr[j], a.Arr[i]
+    a.ArrWeight[i], a.ArrWeight[j] = a.ArrWeight[j], a.ArrWeight[i]
+    a.ArrLabel[i], a.ArrLabel[j] = a.ArrLabel[j], a.ArrLabel[i]
 }
 
 func (a *ArrHeap) Push(x interface{}) {
-    xInt := x.(int)
-    a.Arr = append(a.Arr, xInt)
+    weightLabel := x.(WeightLabel)
+    a.ArrWeight = append(a.ArrWeight, weightLabel.Weight)
+    a.ArrLabel = append(a.ArrLabel, weightLabel.Label)
 }
 
 func (a *ArrHeap) Pop() interface{} {
-    oldLen := len(a.Arr)
+    oldLen := len(a.ArrWeight)
 
-    lastIndx := a.Arr[oldLen-1]
-    lastElm := a.Arr[lastIndx-1]
+    lastIndx := a.ArrWeight[oldLen-1]
+    lastElm := a.ArrWeight[lastIndx-1]
+    lastLabel := a.ArrLabel[lastIndx-1]
 
-    a.Arr = a.Arr[0 : oldLen-1]
-    return lastElm
+    a.ArrWeight = a.ArrWeight[0 : oldLen-1]
+    a.ArrLabel = a.ArrLabel[0 : oldLen-1]
+    return WeightLabel{lastElm, lastLabel}
 }
 
 func ReadTextfile(filepath string) ArrHeap {
@@ -42,9 +48,10 @@ func ReadTextfile(filepath string) ArrHeap {
     var arrHeap = ArrHeap{}
 
     for lineIndx, intStr := range strings.Split(string(contentBytes), "\n") {
-        if arrHeap.Arr == nil {
+        if arrHeap.ArrWeight == nil {
             numNodes, _ := strconv.Atoi(intStr)
-            arrHeap.Arr = make([]int, numNodes)
+            arrHeap.ArrWeight = make([]int, numNodes)
+            arrHeap.ArrLabel = make([]string, numNodes)
             continue
         }
 
@@ -53,7 +60,8 @@ func ReadTextfile(filepath string) ArrHeap {
         }
 
         num, _ := strconv.Atoi(intStr)
-        arrHeap.Arr[lineIndx-1] = num
+        arrHeap.ArrWeight[lineIndx-1] = num
+        arrHeap.ArrLabel[lineIndx-1] = fmt.Sprintf("%d", lineIndx)
     }
 
     return arrHeap
---
+++ week3/week3-ps/ArrHeap/WeightLabel.go
@@ -0,0 +1,6 @@
+package ArrHeap
+
+type WeightLabel struct {
+    Weight int
+    Label  string
+}
