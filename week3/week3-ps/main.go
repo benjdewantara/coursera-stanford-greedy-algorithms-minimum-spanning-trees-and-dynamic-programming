6e1d43801dcbb35d2c4e18fba6e2682f33b0aa0c
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-18 21:31:46 +0700

    Week 3 No 1 Refactored main1

--- week3/week3-ps/main.go
+++ week3/week3-ps/main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-    "./ArrHeap"
-    "container/heap"
-    "fmt"
+    "./Main1"
 )
 
 func main() {
-    a := ArrHeap.ReadTextfile("_eed1bd08e2fa58bbe94b24c06a20dcdb_huffman.txt")
-
-    heap.Init(&a)
-    for a.Len() >= 2 {
-        a.MergeTwoLeastWeight()
-    }
-
-    maxDepth := -1
-    minDepth := -1
-
-    for i := 0; i < len(a.Depths); i++ {
-        if i == 0 {
-            maxDepth, minDepth = a.Depths[i], a.Depths[i]
-            continue
-        }
-
-        if a.Depths[i] < minDepth {
-            minDepth = a.Depths[i]
-        }
-
-        if maxDepth < a.Depths[i] {
-            maxDepth = a.Depths[i]
-        }
-    }
-
-    fmt.Printf("maxDepth = %d\n", maxDepth)
-    fmt.Printf("minDepth = %d\n", minDepth)
+    Main1.Main1()
 }
