2cc634296d5c701e7a052ffbcb14f29378cc4822
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-22 23:50:26 +0700

    Week 4 No 1 Solution so far for no 1

--- week4/week4-ps/Knapsack/Knapsack.go
+++ week4/week4-ps/Knapsack/Knapsack.go
@@ -1,6 +1,7 @@
 package Knapsack
 
 import (
+	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -10,10 +11,64 @@ type Knapsack struct {
 	MaxCapacity int
 	Values      []int
 	Weights     []int
+	Terms       map[string]int
+}
+
+func (k *Knapsack) Optimal() int {
+	optimalValue := -1
+	for i := 0; i < k.Len(); i++ {
+		for x := 0; x <= k.MaxCapacity; x++ {
+			term := k.getTerm(i, x)
+			if optimalValue == -1 {
+				optimalValue = term
+				continue
+			}
+
+			if optimalValue < term {
+				optimalValue = term
+			}
+		}
+	}
+
+	return optimalValue
+}
+
+func TermKeyString(i int, x int) string {
+	return fmt.Sprintf("%d,%d", i, x)
+}
+
+func (k *Knapsack) Len() int {
+	return len(k.Weights)
+}
+
+func (k *Knapsack) getTerm(i int, x int) int {
+	term, exists := k.Terms[TermKeyString(i, x)]
+	if exists {
+		return term
+	}
+
+	if i < 0 {
+		return 0
+	}
+
+	option1 := k.getTerm(i-1, x)
+	option2 := 0
+	if k.Weights[i] <= x {
+		option2 = k.getTerm(i-1, x-k.Weights[i]) + k.Values[i]
+	}
+	maxOption := option1
+
+	if maxOption < option2 {
+		maxOption = option2
+	}
+
+	k.Terms[TermKeyString(i, x)] = maxOption
+	return maxOption
 }
 
 func ReadTextfile(filepath string) Knapsack {
 	k := Knapsack{}
+	k.Terms = make(map[string]int)
 
 	contentBytes, _ := ioutil.ReadFile(filepath)
 	for lineIndx, intStr := range strings.Split(string(contentBytes), "\n") {
--- week4/week4-ps/main.go
+++ week4/week4-ps/main.go
@@ -7,5 +7,7 @@ import (
 
 func main() {
 	k := Knapsack.ReadTextfile("_6dfda29c18c77fd14511ba8964c2e265_knapsack1.txt")
-	fmt.Println(k)
+	//k := Knapsack.ReadTextfile("test_knapsack1.txt")
+	optimal := k.Optimal()
+	fmt.Printf("optimal = %d\n", optimal)
 }
